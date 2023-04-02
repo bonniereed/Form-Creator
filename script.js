8638a725abb3806c24fd96d6de6e9a4e1f9cac53
bonniereed <bonniejeanreed@gmail.com>
2023-04-02 15:05:43 -0500

    added html and css files. Updated html to use javascript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-function Model() {
-    this.title = `Form Creator`;
-    this.inputType = [{}];
-}
+let title = document.querySelector("input-options-container")
+let dropdown = document.querySelector("selector-container)
+const save = document.querySelector("save-container")
