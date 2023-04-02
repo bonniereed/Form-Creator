e33f619b4f91cf719f84e69ff436f420c5989b14
bonniereed <bonniejeanreed@gmail.com>
2023-04-02 15:33:46 -0500

    added javascript

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let title = document.querySelector("input-options-container")
-let dropdown = document.querySelector("selector-container)
-const save = document.querySelector("save-container")
+let title = document.querySelector("input-options-container");
+let dropdown = document.querySelector("selector-container");
+const save = document.querySelector("save-container");
+
+const selectInput = ["radio", "checkbox", "text"];
+
+selectInput.forEach((selector, options) => {
+    const inputEl = document.createElement("div");
+    inputEl.setAttribute("id", "options-", selector);
+    dropdown.append(inputEl);
+});
