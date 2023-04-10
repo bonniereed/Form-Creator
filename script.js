47deb5c1823b74e62a7f7d1f203d8fb39fdb99e4
bonniereed <bonniejeanreed@gmail.com>
2023-04-10 12:46:43 -0500

    working through appending text input

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.getElementById("generate").onclick = function () {
     var options = ["checkbox", "select one", "text"];
-
     var select = document.createElement("select");
     select.name = "option";
     select.id = "option";
@@ -14,7 +13,22 @@ document.getElementById("generate").onclick = function () {
 
     var h3 = document.createElement("h3");
     h3.innerHTML = "Select your input type: ";
+    input = document.createElement("input").setAttribute("text");
     h3.htmlFor = "option";
-
-    document.getElementById("container").appendChild(h3).appendChild(select);
+    document
+        .getElementById("container")
+        .appendChild(h3)
+        .appendChild(input)
+        .appendChild(select);
 };
+
+// switch (expression) {
+//     case x:
+//         // code block
+//         break;
+//     case y:
+//         // code block
+//         break;
+//     default:
+//     // code block
+// }
